2e70db36944c98ada8ac994e106eb459aec6ff84
Bob Reselman <reselbob@gmail.com>
2019-10-04 11:55:47 -0700

    Using async library

--- wordstream/producer/index.js
+++ wordstream/producer/index.js
@@ -1,16 +1,16 @@
 const redis = require('redis');
+const faker = require('faker');
+const async = require('async');
 const uuidv4 = require('uuid/v4');
 const id = uuidv4();
-const faker = require('faker');
 
-const streamName = process.env.WORDSTREAM_STREAM_NAME || 'WORDSTREAM';
-const streamGroup = process.env.WORDSTREAM_CONSUMER_GROUP || 'WORDSTREAM_GROUP';
+
 process.env.REDIS_PORT = process.env.REDIS_PORT || 6379; //yeah, it's a hack
 process.env.REDIS_HOST = process.env.REDIS_HOST || 'localhost'; //yeah, it's a hack
 process.env.REDIS_PWD = process.env.REDIS_PWD || 'none'; //yeah, it's a hack;
 
 //const url = {url: `//${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`};
-const producer = redis.createClient(process.env.REDIS_PORT,process.env.REDIS_HOST);
+const producer = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
 
 /*
 To get docker up and running in an environment in which Docker is installed:
@@ -18,22 +18,23 @@ To get docker up and running in an environment in which Docker is installed:
 docker run -d --name redis-master -p 6379:6379 redis
 
  */
+const streamName = process.env.WORDSTREAM_STREAM_NAME || 'WORDSTREAM';
+const streamGroup = process.env.WORDSTREAM_CONSUMER_GROUP || 'WORDSTREAM_GROUP';
 console.log(`Spinning up producer ${id} for stream ${streamName} at ${new Date()}`);
-
-producer.xgroup('CREATE', streamName, streamGroup, '$', function (err) {
-    if (err) {
-        console.error(`The group ${streamGroup} exists.`);
-    }
-    console.log(`Stream: ${streamName} created group: ${streamGroup}`);
-    const timeout = setInterval(function () {
+async.forever(
+    function (next) {
         const fieldName = 'Word';
         const str = `${faker.random.words(1)} : ${new Date()}`;
         producer.xadd(streamName, '*', fieldName, str, function (err) {
             console.log(`Producer ${id} is adding message: ${str} to field: ${fieldName} at stream: ${streamName}`);
-            if (err) {
-                console.error(err);
-            }
-        });
-    }, 1000);
-});
+            next();
+        }),
+            function (err) {
+                if (!err.message.includes('BUSYGROUP')) {
+                    err.isShuttingDown = true;
+                    console.error(err.message);
+                    process.exit(9)
+                }
+            };
+    });
 
