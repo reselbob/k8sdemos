4d49f4f47fd3f414937bd92728e607d6f3ac8f14
Bob Reselman <reselbob@gmail.com>
2019-10-04 12:34:20 -0700

    reverting consumer back to setInterval

--- wordstream/consumer/index.js
+++ wordstream/consumer/index.js
@@ -7,7 +7,6 @@ docker run -d --name redis-master -p 6379:6379 redis
 const redis = require('redis');
 const uuidv4 = require('uuid/v4');
 const consumerId = uuidv4();
-const async = require('async');
 
 const streamName = process.env.WORDSTREAM_STREAM_NAME || 'WORDSTREAM';
 const streamGroup = process.env.WORDSTREAM_CONSUMER_GROUP || 'WORDSTREAM_GROUP';
@@ -17,33 +16,23 @@ process.env.REDIS_HOST = process.env.REDIS_HOST || 'localhost'; //yeah, it's a h
 process.env.REDIS_PWD = process.env.REDIS_PWD || 'none'; //yeah, it's a hack;
 
 //const url = {url: `//${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`};
-const client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
+const client = redis.createClient(process.env.REDIS_PORT,process.env.REDIS_HOST);
 //const client = redis.createClient(url);
 console.log(`Spinning up consumer ${consumerId} for stream ${streamName} at ${new Date()}`);
-async.forever(
-    function (next) {
-        client.xgroup('CREATE', streamName, streamGroup, '$', function (err) {
-            if (!err.message.includes('BUSYGROUP')) {
-                console.error(err.message);
+
+const timeout = setInterval(function () {
+    client.xgroup('CREATE', streamName, streamGroup, '$', function (err) {
+        if (err) {
+            if(!err.message.includes('BUSYGROUP')){
+                console.error(err.message)
             }
-            client.xreadgroup('GROUP', streamGroup, consumerId, 'BLOCK', 1000, 'COUNT', 1, 'NOACK',
-                'STREAMS', streamName, '>', function (err, stream) {
-                    if (err) {
-                        return console.error(err);
-                    }
-                    console.log(JSON.stringify({consumerId, stream}));
-                    next();
-                });
-        })
-    },
-    function (err) {
-        if (!err.message.includes('BUSYGROUP')) {
-            err.isShuttingDown = true;
-            console.error(err.message);
-            process.exit(9)
         }
-    }
-);
-
-
-
+        client.xreadgroup('GROUP', streamGroup, consumerId, 'BLOCK', 1000, 'COUNT', 1, 'NOACK',
+            'STREAMS', streamName, '>', function (err, stream) {
+                if (err) {
+                    return console.error(err);
+                }
+                console.log(JSON.stringify({consumerId, stream}));
+            });
+    });
+}, 250);
